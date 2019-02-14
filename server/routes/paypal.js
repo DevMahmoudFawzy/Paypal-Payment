1e72ee129d995349e425ad59e00b0246c35ba585
Mahmoud Fawzy <dev.mahmoudfawzy@gmail.com>
2019-02-14 13:37:02 +0200

    paypal service added

--- server/routes/paypal.js
+++ server/routes/paypal.js
@@ -1,70 +1,23 @@
 const express = require('express');
-const fetch = require('node-fetch');
-const base64 = require('base-64');
 const router = express.Router();
+
+const { AuthenticateApp, CreatePayment } = require('../services/PaypalService');
 
 router.get('/pay', (req, res) => {
     res.send('asdasd');
 });
 
-router.post('/pay', (req, res) => {
+router.post('/pay', async (req, res) => {
 
-    const username = 'AUN7VSNweEDzKHyZOQwERvZ4xR-LXcPU2UNPorGfwOYMguRIouCfD0qQKE9qw6YVIKd0BR0Te2BggMuO';
-    const password = 'EKXQsXChDc70h6XgRDZ-bIaftMPVoNCa2JmEvDWs6BK2GGPOh-oZCsQLAQn7L71ug3xGjMgdJacVlZZs';
-    fetch('https://api.sandbox.paypal.com/v1/oauth2/token', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'Accept': 'application/json',
-            'Accept-Language': 'en_US',
-            'Authorization': 'Basic ' + base64.encode(username + ":" + password)
-        },
-        body: "grant_type=client_credentials"
-    })
-        .then(res => res.json())
-        .then(reply => {
-            const token = reply.access_token;
-
-            fetch('https://api.sandbox.paypal.com/v1/payments/payment', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Accept': 'application/json',
-                    'Accept-Language': 'en_US',
-                    'Authorization': 'Bearer ' + token
-                },
-                body: `{
-                    "intent": "sale",
-                    "redirect_urls": {
-                        "return_url": "http://localhost:4200/payment/success",
-                        "cancel_url": "http://localhost:4200/payment/error"
-                    },
-                    "payer": {
-                        "payment_method": "paypal"
-                    },
-                    "transactions": [{
-                        "amount": {
-                            "total": "7.47",
-                            "currency": "USD"
-                        }
-                    }]
-                }`
-            })
-                .then(response => response.json())
-                .then(reply => {
-                    const approval_url = reply.links.find(a => a.rel === 'approval_url').href;
-                    console.log(approval_url);
-                    return res.send({ status: true, url: approval_url });
-                })
-                .catch(err => {
-                    console.log(err);
-                    return res.status(500).send({ status: false, message: 'server error!!' });
-                });
-        })
-        .catch(err => {
-            console.log(err);
-            return res.status(500).send({ status: false, message: 'server error!!' });
-        });
+    try {
+        const accessToken = await AuthenticateApp();
+        if (accessToken) {
+            const approvalURL = await CreatePayment(accessToken, req.get('origin'));
+            return res.send({ status: true, ApprovalURL: approvalURL });
+        }
+    } catch (error) {
+        return res.status(500).send({ status: false, message: 'server error!!' });
+    }
 });
 
 module.exports = router;
---
+++ server/services/PaypalService.js
@@ -0,0 +1,70 @@
+const fetch = require('node-fetch');
+const base64 = require('base-64');
+
+function authenticateApp() {
+    const username = 'AUN7VSNweEDzKHyZOQwERvZ4xR-LXcPU2UNPorGfwOYMguRIouCfD0qQKE9qw6YVIKd0BR0Te2BggMuO';
+    const password = 'EKXQsXChDc70h6XgRDZ-bIaftMPVoNCa2JmEvDWs6BK2GGPOh-oZCsQLAQn7L71ug3xGjMgdJacVlZZs';
+
+    return new Promise((resolve, reject) => {
+        fetch('https://api.sandbox.paypal.com/v1/oauth2/token', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Accept': 'application/json',
+                'Accept-Language': 'en_US',
+                'Authorization': 'Basic ' + base64.encode(username + ":" + password)
+            },
+            body: "grant_type=client_credentials"
+        })
+            .then(res => res.json())
+            .then(reply => { console.log('200 success!!'); resolve(reply.access_token) })
+            .catch(err => {
+                reject(err);
+            });
+    });
+}
+
+function createPayment(appAccessToken, origin) {
+    return new Promise((resolve, reject) => {
+        fetch('https://api.sandbox.paypal.com/v1/payments/payment', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + appAccessToken
+            },
+            body: `{
+                    "intent": "sale",
+                    "payer": {
+                        "payment_method": "paypal"
+                    },
+                    "transactions": [{
+                        "amount": {
+                            "total": "7.47",
+                            "currency": "USD"
+                        }
+                    }],
+                    "redirect_urls": {
+                        "return_url": "${origin}/payment/success",
+                        "cancel_url": "${origin}/payment/error"
+                    },
+                }`
+        })
+            .then(response => response.json())
+            .then(reply => {
+                const approval_url = reply.links.find(a => a.rel === 'approval_url').href;
+                resolve(approval_url);
+                // console.log(approval_url);
+                // return res.send({ status: true, url: approval_url });
+            })
+            .catch(err => {
+                // console.log(err);
+                reject(err);
+                // return res.status(500).send({ status: false, message: 'server error!!' });
+            });
+    });
+}
+
+module.exports = {
+    AuthenticateApp: authenticateApp,
+    CreatePayment: createPayment
+};
